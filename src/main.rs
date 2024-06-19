952fd5577c5be4bd5c8b870cf9cb22c61a835825
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-06-19 06:44:03 +0200

    API refinements
    Entities identified by path: we can PUT a hash of attributes and values
    to them, and GET them.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,13 @@ use parking_lot::RwLock;
 use std::sync::Arc;
 use std::collections::HashMap;
 
-type Items = HashMap<String, HashMap<String, String>>;
+type Entity = HashMap<String, String>;
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
-struct Item {
-    entity: String,
-    attribute: String,
-    value: String,
-}
+type Entities = HashMap<String, Entity>;
 
 #[derive(Clone)]
 struct Store {
-    store: Arc<RwLock<Items>>
+    store: Arc<RwLock<Entities>>
 }
 
 impl Store {
@@ -26,26 +21,41 @@ impl Store {
     }
 }
 
-async fn add_entry(
-    item: Item,
+async fn create_entity(
+    key: String,
+    entity: Entity,
     store: Store
     ) -> Result<impl warp::Reply, warp::Rejection> {
-    store.store.write().entry(item.entity).or_insert(HashMap::new()).insert(item.attribute, item.value);
+    store.store.write().insert(key, entity);
     Ok(warp::reply::with_status(
             "OK",
             http::StatusCode::CREATED,
             ))
 }
 
-async fn get_entries(
+async fn get_entities(
     store: Store
     ) -> Result<impl warp::Reply, warp::Rejection> {
         let result = store.store.read();
         Ok(warp::reply::json(&*result))
 }
 
+async fn get_entity(
+    key: String,
+    store: Store
+    ) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+        let store_lock = store.store.read();
+        let result = store_lock.get(&key);
+        match result {
+            Some(entity) => Ok(Box::new(warp::reply::json(&*entity))),
+            None => Ok(Box::new(warp::reply::with_status(
+                "Not found",
+                http::StatusCode::NOT_FOUND
+                ))),
+        }
+}
 
-fn json_body() -> impl Filter<Extract = (Item,), Error = warp::Rejection> + Clone {
+fn json_body() -> impl Filter<Extract = (Entity,), Error = warp::Rejection> + Clone {
     // When accepting a body, we want a JSON body
     // (and to reject huge payloads)...
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
@@ -56,18 +66,27 @@ async fn main() {
     let store = Store::new();
     let store_filter =  warp::any().map(move || store.clone());
 
-    let add_item = warp::post()
+    let create_entity_route = warp::put()
+        .and(warp::path::param())
         .and(warp::path::end())
         .and(json_body())
         .and(store_filter.clone())
-        .and_then(add_entry);
+        .and_then(create_entity);
 
-    let get_items = warp::get()
+    let get_entities_route = warp::get()
         .and(warp::path::end())
         .and(store_filter.clone())
-        .and_then(get_entries);
+        .and_then(get_entities);
 
-    let routes = add_item.or(get_items);
+    let get_entity_route = warp::get()
+        .and(warp::path::param())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(get_entity);
+
+    let routes = create_entity_route
+        .or(get_entities_route)
+        .or(get_entity_route);
 
     warp::serve(routes)
         .run(([127,0,0,1], 3030))
