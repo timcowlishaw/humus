45f78dbec9b612cef1acf91dda61e5bf97c0a92a
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-10-09 08:57:12 +0200

    build script improvements

--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
-VERSION=`cargo read-manifest | jq .version`
+echo "Checking git committed and pushed..."
+test -z "$(git status --porcelain)"
+git merge-base --is-ancestor HEAD @{u}
+VERSION=`cargo read-manifest | jq -r .version`
 echo "Building release, version. $VERSION".
 echo "Tagging..."
 git tag v$VERSION
@@ -9,8 +12,8 @@ echo "Building release artefacts..."
 cargo build --release
 cross build --release --target x86_64-pc-windows-gnu
 echo "Publishing docker image.."
-docker build -t timcowlishaw/humus:latest timcowlishaw/humus:$VERSION
+docker build -t timcowlishaw/humus:latest -t timcowlishaw/humus:$VERSION .
 docker push timcowlishaw/humus:latest timcowlishaw/humus:$VERSION
 echo "Publishing crate..."
-cargo publish
+cargo publish --dry-run
 echo "Done! Remember to create Github releases with the compiled artefacts"
